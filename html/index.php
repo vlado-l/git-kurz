4f76e1dbf913d9d55814a37494f454f2f3948269
Vladislav Ladický <vladol@itl.lan>
2020-08-23 17:41:12 +0200

    initial commit

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<?php
+  /* require "../vendor/autoload.php";
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load(); */
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title><?php /* echo $_ENV["APP_NAME"]; */ ?></title>
+    <link rel="stylesheet" href="css/myweb.css">
+  </head>
+
+  <body>
+    <main class="full-page">
+      <h1 class="text-center">Initial content</h1>
+    </main> 
+  </body>
+</html>
